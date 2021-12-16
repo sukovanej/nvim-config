9e1c37bbcd84ba562901f37ae88b3111cdb6f620
milan.suk <milan.suk@kiwi.com>
2021-12-16 22:17:21 +0100

    hard-mode for Standa activated

--- lua/rust_custom.lua
+++ lua/rust_custom.lua
@@ -1,1 +1,13 @@
-require('rust-tools').setup({})
+require('rust-tools').setup({
+  inlay_hints = {
+    -- wheter to show parameter hints with the inlay hints or not
+    show_parameter_hints = true,
+
+    -- prefix for parameter hints
+    parameter_hints_prefix = ":: ",
+
+    -- prefix for all the other hints (type, chaining)
+    other_hints_prefix = ":: ",
+    right_align = false,
+  }
+})
