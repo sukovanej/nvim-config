720b8abfd332c15a0d29c491f454f93ad9b093c1
milan.suk <milan.suk@kiwi.com>
2021-12-20 17:46:02 +0100

    feat(lualine): show relative path of the file

--- lua/theme.lua
+++ lua/theme.lua
@@ -3,6 +3,15 @@ vim.cmd [[set t_Co=256]]
 require'lualine'.setup {
   options = {
     theme = 'auto'
+  },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      }
+    }
   }
 }
 
