ec55d942b4eb79db5be8ecf01c4d332609f0c9d9
Milan Suk <Milansuk@email.cz>
2022-04-17 12:40:05 +0200

    feat: open neogit status in split window

--- lua/git_integration.lua
+++ lua/git_integration.lua
@@ -3,5 +3,5 @@ require("gitsigns").setup { current_line_blame = true }
 
 vim.cmd [[nnoremap <silent> <space>gr <cmd>Gitsigns reset_hunk<CR>]]
 vim.cmd [[vnoremap <silent> <space>gr <cmd>'<,'>Gitsigns reset_hunk<CR>]]
-vim.cmd [[nnoremap <silent> <space>gs <cmd>Neogit<CR>]]
+vim.cmd [[nnoremap <silent> <space>gs <cmd>lua require("neogit").open({ kind = "split" })<CR>]]
 vim.cmd [[nnoremap <silent> <space>gc <cmd>Neogit commit<CR>]]
