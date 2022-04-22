456238054e45f4f0b7da77af6a85c77e3c44337a
Milan Suk <Milansuk@email.cz>
2022-04-22 22:48:13 +0200

    fix: tree-sitter ensure 'all'

--- lua/tree_sitter.lua
+++ lua/tree_sitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup {
   highlight = { enable = true },
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   -- indent doesn't work well for me for Python
   -- indent = { enable = true },
   rainbow = { enable = true },
