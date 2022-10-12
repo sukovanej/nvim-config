06d73798d5e42e9a21eabb94d8bb7fbb39b7a90f
Milan Suk <Milansuk@email.cz>
2022-10-12 22:06:27 +0200

    feat: get the tabline back

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,5 +1,14 @@
 vim.cmd [[set t_Co=256]]
 
+require("tabline").setup {
+  enable = false,
+  options = {
+    show_filename_only = false,
+    modified_icon = "+ ",
+    modified_italic = true,
+  },
+}
+
 require("lualine").setup {
   options = {
     theme = "tokyonight",
@@ -12,6 +21,10 @@ require("lualine").setup {
         path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
       },
     },
+  },
+  tabline = {
+    lualine_c = { require("tabline").tabline_buffers },
+    lualine_x = { require("tabline").tabline_tabs },
   },
   winbar = {
     lualine_a = {},
