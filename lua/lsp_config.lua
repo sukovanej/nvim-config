b54a45d50ec268b97343673b9c072f71daa1c95b
Milan Suk <Milansuk@email.cz>
2022-10-16 15:46:06 +0200

    feat: use default_capabilities instead of update_capabilities

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -7,7 +7,7 @@ local lspconfig = require "lspconfig"
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 mason_lspconfig.setup_handlers {
   -- The first entry (without a key) will be the default handler
