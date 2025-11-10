ee32adc6c2a0e22037551f7bbd7456ba17b29bf6
Milan Suk <Milansuk@email.cz>
2025-11-10 13:34:48 +0100

    blink documentation

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -28,6 +28,13 @@ setup.opts = {
       auto_show = function(ctx)
         return ctx.mode ~= "cmdline"
       end,
+      draw = {
+        columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 }, { "source_name" } },
+      },
+    },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 200,
     },
   },
 }
--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -7,9 +7,38 @@ setup.dependencies = {
 }
 
 function setup.config(_)
+  local capabilities = require("blink.cmp").get_lsp_capabilities()
+
   require("mason").setup()
   require("mason-lspconfig").setup {
     ensure_installed = { "lua_ls", "ty" },
+    handlers = {
+      -- Default handler for all servers
+      function(server_name)
+        require("lspconfig")[server_name].setup {
+          capabilities = capabilities,
+        }
+      end,
+
+      -- Custom handler for lua_ls to avoid workspace warnings
+      ["lua_ls"] = function()
+        require("lspconfig").lua_ls.setup {
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+              },
+              telemetry = { enable = false },
+            },
+          },
+        }
+      end,
+    },
   }
 end
 
